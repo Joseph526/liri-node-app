0cc6b57f3dc4069f5c6df2dd2318ef235a86e10c
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-04-15 23:57:41 -0400

    Working on movie-this function

--- liri.js
+++ liri.js
@@ -12,6 +12,29 @@ var client = new Twitter(keys.twitter);
 // Capture input from CLI
 var args = process.argv[2];
 
+// Declare functions
+var liri = {
+    movieThis: function() {
+        var movieName = process.argv[3];
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+        request(queryUrl, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var movieInfo = JSON.parse(body);
+                console.log(
+                    "Title: " + movieInfo.Title + 
+                    "\nYear: " + movieInfo.Year +
+                    "\n" + movieInfo.Ratings[0].Source + " rating: " + movieInfo.Ratings[0].Value +
+                    "\n" + movieInfo.Ratings[1].Source + " rating: " + movieInfo.Ratings[1].Value +
+                    "\nCountry: " + movieInfo.Country +
+                    "\nLanguage: " + movieInfo.Language +
+                    "\nPlot: " + movieInfo.Plot +
+                    "\nActors: " + movieInfo.Actors
+                );
+            }
+        });
+    }
+};
+
 // Execute appropriate switch case
 switch (args) {
     case "my-tweets":
@@ -21,7 +44,8 @@ switch (args) {
         console.log("Spotify this");
         break;
     case "movie-this":
-        console.log("Movie this");
+        console.log("==========\nMovie this\n==========");
+        liri.movieThis();
         break;
     case "do-what-it-says":
         console.log("Do what it says");
