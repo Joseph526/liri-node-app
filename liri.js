9c3610a7ae0b41bb62ec60be2302bad48e34ef07
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-04-15 23:21:42 -0400

    Added switch statement

--- liri.js
+++ liri.js
@@ -1,9 +1,31 @@
 // Declare global requirements
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var Twitter = require("twitter");
+var request = require("request");
 
+// Declare new instance of each API
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 // Capture input from CLI
-var args = process.argv;
+var args = process.argv[2];
+
+// Execute appropriate switch case
+switch (args) {
+    case "my-tweets":
+        console.log("Twitter function under construction");
+        break;
+    case "spotify-this-song":
+        console.log("Spotify this");
+        break;
+    case "movie-this":
+        console.log("Movie this");
+        break;
+    case "do-what-it-says":
+        console.log("Do what it says");
+        break;
+    default:
+        console.log("Command not defined");
+}
