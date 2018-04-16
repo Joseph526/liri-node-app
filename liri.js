96489bd33e6b4f9483428dda71b213fba173722d
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-04-15 22:41:57 -0400

    Building app structure

--- liri.js
+++ liri.js
@@ -1,2 +1,9 @@
+// Declare global requirements
 require("dotenv").config();
 var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+// Capture input from CLI
+var args = process.argv;
